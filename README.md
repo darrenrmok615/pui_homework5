e7c49b84ed6405169827bb7e47bdce0351ab2ce0
Darren Mok <darrenmok@Darrens-MacBook-Pro.local>
2021-03-31 15:36:41 +0800

    Updated shop and product detail pages + JS functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# pui_homework5
+# pui_homework6a
 
-Inside this folder contains the HTML and CSS files for the following pages on the BunBunBakeShop prototype: Home page, Shop page (Product browsing + product detail page), and the About page. The Home page utilizes a animation for the "Try out Original..." button found from this tutorial video: https://www.youtube.com/watch?v=A1KP6yrCP0c. All other code is entirely my own.
+Inside this folder contains the HTML and CSS files for the following pages on the BunBunBakeShop prototype: Home page, Shop page (Product browsing + product detail page), About page and cart page. The Home page utilizes a animation for the "Try out Original..." button found from this tutorial video: https://www.youtube.com/watch?v=A1KP6yrCP0c. Referenced https://www.youtube.com/watch?v=tEAl7L62GEw for javascript code. Also checked out example carts from: https://speckyboy.com/free-shopping-cart-css-javascript/. All other code is entirely my own.
+
+Spoke to Meghan during office hours about using homework 5 repository since I didn't know at the time we were supposed to make a new one. She mentioned that this was OK, but to leave a note in here.
--- main.js
+++ main.js
@@ -1,50 +1,83 @@
 let carts = document.querySelectorAll('.add_to_cart');
+let detail = document.querySelectorAll('.roll');
 
+/* declaring products */
 let products = [
 	{
 		name: 'Original',
 		tag: 'Original',
-		price: 5.99 ,
-		inCart: 0
+		glaze: 'none',
+		rolls: 0,
+		description: "Our signature original cinnamon bun! Delivered to your door within the hour, guarenteed fresh and warm! We promise you won't be able to stop eating them!",
+		price: '$5.99',
+		inCart: 0,
+		img: document.getElementById("original")
 	},
 	{
 		name: 'Blackberry',
 		tag: 'Blackberry',
-		price: 6.99 ,
-		inCart: 0
+		glaze: 'none',
+		rolls: 0,
+		description: "Our blackberry cinnamon bun! Delivered to your door within the hour, guarenteed fresh and warm! We promise you won't be able to stop eating them!",
+		price: '$6.99',
+		inCart: 0,
+		img: document.getElementById("blackberry")
 	},
 	{
 		name: 'Walnut',
 		tag: 'Walnut',
-		price: 6.99 ,
-		inCart: 0
+		glaze: 'none',
+		rolls: 0,
+		description: "Our walnut cinnamon bun! Delivered to your door within the hour, guarenteed fresh and warm! We promise you won't be able to stop eating them!",
+		price: '$6.99',
+		inCart: 0,
+		img: document.getElementById("walnut")
 	},
 	{
-		name: 'Gluten-Free',
-		tag: 'Gluten-Free',
-		price: 5.99 ,
-		inCart: 0
+		name: 'Original Gluten-Free',
+		tag: 'Orginal Gluten-Free',
+		glaze: 'none',
+		rolls: 0,
+		description: "Our original gluten-free cinnamon bun! Delivered to your door within the hour, guarenteed fresh and warm! We promise you won't be able to stop eating them!",
+		price: '$5.99',
+		inCart: 0,
+		img: document.getElementById("gluten")
 	},
 	{
 		name: 'Pumpkin Spice',
 		tag: 'Pumpkin Spice',
-		price: 7.99 ,
-		inCart: 0
+		glaze: 'none',
+		rolls: 0,
+		description: "Our pumpkin spice cinnamon bun! Delivered to your door within the hour, guarenteed fresh and warm! We promise you won't be able to stop eating them!",
+		price: '$7.99',
+		inCart: 0,
+		img: document.getElementById("pumpkin")
 	},
 	{
 		name: 'Caramel Pecan',
 		tag: 'Caramel Pecan',
-		price: 7.99 ,
-		inCart: 0
+		glaze: 'none',
+		rolls: 0,
+		description: "Our caramel pecan cinnamon bun! Delivered to your door within the hour, guarenteed fresh and warm! We promise you won't be able to stop eating them!",
+		price: '$7.99',
+		inCart: 0,
+		img: document.getElementById("caramel")
 	},
 ]
 
 for (let i=0; i < carts.length; i++) {
 	carts[i].addEventListener('click',() => {
-		cartCount();
+		cartCount(products[i]);
 	})
 }
 
+for (let i=0; i < detail.length; i++) {
+	detail[i].addEventListener('click', () => {
+		setDetail(products[i]);
+	})
+}
+
+/* ensures that the cart # indicator loads properly */
 function loadCartCount() {
 	let productNumber = localStorage.getItem('cartCount');
 
@@ -53,7 +86,8 @@ function loadCartCount() {
 	}
 }
 
-function cartCount() {
+/* updates cart # indicator */
+function cartCount(product) {
 	let productNumber = localStorage.getItem('cartCount');
 	productNumber = parseInt(productNumber);
 
@@ -65,6 +99,68 @@ function cartCount() {
 		localStorage.setItem('cartCount', 1);
 		document.querySelector('.cart_icon span').textContent = 1;
 	}
+
+	/* setItems(product); */
 }
 
+/* updates product details page with correct details */
+function setDetail(product) {
+	let detailName = localStorage.setItem("itemName", product.name);
+	let detailPrice = localStorage.setItem("itemPrice", product.price);
+	let detailDescription = localStorage.setItem("itemDescription", product.description);
+	let detailImage = localStorage.setItem("itemImage", product.img);
+	/* console.log("Product is", product.name);
+	let detailItem = localStorage.getItem('product_selected');
+	detailItem = JSON.parse(detailItem);
+
+	detailItem = {
+		[product.tag]: product
+	}
+	
+	localStorage.setItem('product_selected',JSON.stringify(detailItem)); */
+}
+
+function loadDetail() {
+	let detailName = localStorage.getItem("itemName");
+	let detailPrice = localStorage.getItem("itemPrice");
+	let detailDescription = localStorage.getItem("itemDescription");
+	let detailImage = localStorage.getItem("itemImage");
+
+	document.querySelector('.left span').textContent = "Flavor: " + detailName;
+	document.querySelector('.right span').textContent = detailDescription;
+	document.querySelector('.price span').textContent = detailPrice;
+
+	let product_image = document.querySelector('.roll_image');
+	if (detailName == "Orginal") {
+		product_image.innerHTML = '<img class="roll" src="images/roll.png" alt="picture of original cinnamon roll product" id="blackberry">';
+	}
+	else if (detailName == "Blackberry") {
+		product_image.innerHTML = '<img class="roll" src="images/roll2.png" alt="picture of blackberry cinnamon roll product" id="blackberry">';
+	}
+	else if (detailName == "Walnut") {
+		product_image.innerHTML = '<img class="roll" src="images/roll3.png" alt="picture of walnut cinnamon roll product" id="blackberry">';
+	}
+	else if (detailName == "Original Gluten-Free") {
+		product_image.innerHTML = '<img class="roll" src="images/roll4.png" alt="picture of original (gluten-free) cinnamon roll product" id="blackberry">';
+	}
+	else if (detailName == "Pumpkin Spice") {
+		product_image.innerHTML = '<img class="roll" src="images/roll5.png" alt="picture of pumpkin spice cinnamon roll product" id="blackberry">';
+	}
+	else if (detailName == "Caramel Pecan") {
+		product_image.innerHTML = '<img class="roll" src="images/roll6.png" alt="picture of carmel pecan cinnamon roll product" id="blackberry">';
+
+	}
+}
+
+/* function setItems(product) {
+	product.inCart = 1;
+	
+	let cartItems = {
+		[product.tag]: product
+	}
+	
+	localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+} */
+
 loadCartCount();
+loadDetail();
